1e5a876dde3337708b63948aef705883a9fb805d
ingalless <jonathanaingall@gmail.com>
2021-10-29 21:21:06 +0100

    Generate points

--- src/sketch/dynamic.clj
+++ src/sketch/dynamic.clj
@@ -5,6 +5,11 @@
   (no-loop)
   (color-mode :hsb 360 100 100 1.0)
   (background 220 49 30)
-  (rect 150 150 200 200)
+  (stroke 255)
+  (doseq [y (range 0 (width) 20)]
+    (begin-shape :points)
+    (doseq [x (range 0 (height) 20)]
+      (vertex x (random y (+ y 10))))
+    (end-shape :close))
   (save "sketch.tif")
   (save "sketch.png"))
